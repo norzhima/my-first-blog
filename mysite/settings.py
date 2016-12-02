249a5cb30ef729e03a71e12a10059e9f1cb61a95
norzhima <chagdurova1992@mail.ru>
2016-12-02 14:43:10 +0300

    Changed the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1cl95e(44h=ktc9hnxzrjq7h+ka07y=ukots=eu155m-o@(zw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['norzhima.pythonanywhere.com']
 
