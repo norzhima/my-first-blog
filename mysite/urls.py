71613676f5b5235e0a201bf903175ff8648f3d84
norzhima <chagdurova1992@mail.ru>
2016-12-05 16:04:21 +0300

    added auth

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
